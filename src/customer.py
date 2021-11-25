947fce66a45001601a0ea5ccbbdf3b669b5e1046
Amna Bashir <amna.bashir2@gmail.com>
2021-11-25 15:11:56 +0000

    favourite performer

--- src/customer.py
+++ src/customer.py
@@ -4,4 +4,11 @@ class Customer:
         self.wallet= wallet
         self.favourite_performer= favourite_performer
 
-        
+    def check_money(self):
+        return self.wallet  
+
+    def reduce_wallet(self, amount):
+        self.wallet -= amount
+
+    def say_favourite_performer(self):
+        return self.favourite_performer
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -14,3 +14,14 @@ class TestCustomer(unittest.TestCase):
 
     def test_customer_has_favouriet_performer(self):
         self.assertEqual(self.performer, self.customer.favourite_performer)
+
+    def test_customer_check_money(self):
+        self.assertEqual(100, self.customer.check_money())
+
+    def test_customer_reduce_wallet(self):
+        self.customer.reduce_wallet(10)
+        self.assertEqual(90, self.customer.check_money())
+
+    def test_customer_says_favourite_performer(self):
+        self.assertEqual(self.performer, self.customer.say_favourite_performer())
+
