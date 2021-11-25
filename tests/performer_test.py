e3a8fc416d002e47b04fd976e9236e3dd8c2841d
Amna Bashir <amna.bashir2@gmail.com>
2021-11-25 14:31:26 +0000

    added increase earning method

--- tests/performer_test.py
+++ tests/performer_test.py
@@ -3,3 +3,13 @@ from src.performer import Performer
 class TestPerformer(unittest.TestCase):
     def setUp(self):
         self.performer= Performer("Jill")
+
+    def test_performer_has_name(self):
+        self.assertEqual("Jill", self.performer.name)
+
+    def test_performer_has_earning(self):
+        self.assertEqual(0,self.performer.earnings)
+    
+    def test_increase_earning(self):
+        self.performer.increase_earnings(50)
+        self.assertEqual(50, self.performer.earnings)
